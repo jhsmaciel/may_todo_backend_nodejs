d4f354db71ea447ba03c2afd92306d619c5a0bc0
João Henrique Maciel <joao.maciel@autbank.com.br>
2020-04-29 20:00:11 -0300

    first commit github

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const port = process.env.PORT || 3333;
 const express = require('express');
 const routes = require('./src/routes');
 const { errors } = require('celebrate');
 const cors = require('cors')
 const app = express();
 
-var port = process.env.PORT || 3333;
+
 
 app.use(cors());
 app.use(express.json());
